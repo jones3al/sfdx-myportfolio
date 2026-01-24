3f675e12b59cd8b95d91a9c12e06668e7ec3cc52
Alex Jones <jones3al@gmail.com>
2026-01-24 12:42:08 -0500

    add bmi calculator logic

--- force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
+++ force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -1,3 +1,50 @@
 import { LightningElement } from 'lwc';
 
-export default class BmiCalculator extends LightningElement {}
+export default class BmiCalculator extends LightningElement {
+     height = ''
+     weight = ''
+     bmiValue = ''
+     result = ''
+
+     //assign height and weight inputs
+     inputHandler(event){
+        const{name, value} = event.target
+        if(name=="height"){
+            this.height = value
+
+        }
+        if(name == "weight"){
+            this.weight = value
+        }
+     }
+     submitHandler(event){
+        event.preventDefault()
+        console.log(this.height)
+        console.log(this.weight)
+        this.calculateBMI()
+     }
+     calculateBMI(){
+
+        //bmi calculation is weight in pounds / (height in inches * height in inches) * 703
+        let bmi = Number(this.weight/(this.height*this.height))*703
+        this.bmiValue = Number(bmi.toFixed(2))
+
+        if(this.bmiValue < 18.5){
+            this.result = "Underweight"
+        }
+        else if(this.bmiValue >= 18.5 && this.bmiValue < 25 ){
+            this.result = "Healthy"
+        }
+        else if(this.bmiValue >= 25 && this.bmiValue < 30){
+            this.result = "Overweight"
+        }
+        else{
+            this.result = "Obese"
+        }
+        console.log("BMI value is: ", bmi)
+        console.log("Result is: ", this.result)
+      }
+      recalculate(){
+         
+      }
+}
